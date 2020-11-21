31bb7ed237b3a52807eecb2b793fffeaba1a9ab9
zhonghong <zhonghong@liangzishidai.cn>
2020-11-21 15:38:16 +0800

    多线程

--- bert_with_lstm/dataset.py
+++ bert_with_lstm/dataset.py
@@ -5,6 +5,7 @@ import random
 import numpy as np
 import os
 import pickle
+import threading
 
 from bert_with_lstm.config import *
 from bert_serving.client import BertClient
@@ -78,7 +79,7 @@ class Dataset(object):
         self.eval_input_example = []
         self.test_input_example = []
 
-    def getTrainData(self):
+    def getTrainData(self, bertClient):
         label_list_file = os.path.join(config.outputPath, "label_list.record")
         train_input_example_file = os.path.join(config.outputPath, "train_input_example.record")
         if os.path.exists(label_list_file) and os.path.exists(train_input_example_file):
@@ -104,19 +105,19 @@ class Dataset(object):
             content = split_line[1]
             if len(content) > config.max_length:
                 for item in get_split_text(content, config.max_length, 0):
-                    embedding = bc.encode(get_split_text(item, config.split_len, config.overlap_len))
+                    embedding = bertClient.encode(get_split_text(item, config.split_len, config.overlap_len))
                     print(embedding.shape)
                     self.train_input_example.append(InputExample(embedding, label=lab))
                     num += 1
             else:
-                embedding = bc.encode(get_split_text(content, config.split_len, config.overlap_len))
+                embedding = bertClient.encode(get_split_text(content, config.split_len, config.overlap_len))
                 self.train_input_example.append(InputExample(embedding, label=lab))
                 num += 1
         # 序列化
         writeDataFile(self.label_list, label_list_file)
         writeDataFile(self.train_input_example, train_input_example_file)
 
-    def getValData(self):
+    def getValData(self, bertClient):
         eval_input_example_file = os.path.join(config.outputPath, "eval_input_example.record")
         if os.path.exists(eval_input_example_file):
             self.eval_input_example = readDataFile(eval_input_example_file)
@@ -138,18 +139,18 @@ class Dataset(object):
             content = split_line[1]
             if len(content) > config.max_length:
                 for item in get_split_text(content, config.max_length, 0):
-                    embedding = bc.encode(get_split_text(item, config.split_len, config.overlap_len))
+                    embedding = bertClient.encode(get_split_text(item, config.split_len, config.overlap_len))
                     print(embedding.shape)
                     self.eval_input_example.append(InputExample(embedding, label=lab))
                     num += 1
             else:
-                embedding = bc.encode(get_split_text(content, config.split_len, config.overlap_len))
+                embedding = bertClient.encode(get_split_text(content, config.split_len, config.overlap_len))
                 self.eval_input_example.append(InputExample(embedding, label=lab))
                 num += 1
         # 序列化
         writeDataFile(self.eval_input_example, eval_input_example_file)
 
-    def getTestData(self):
+    def getTestData(self, bertClient):
         test_input_example_file = os.path.join(config.outputPath, "test_input_example.record")
         if os.path.exists(test_input_example_file):
             self.test_input_example = readDataFile(test_input_example_file)
@@ -167,31 +168,31 @@ class Dataset(object):
                 print("test step ==> ", num, " index ==> ", index, " sum ==> ", len(reader))
             lab = split_line[0]
             content = split_line[1]
-            embedding = bc.encode(get_split_text(content, config.split_len, 0)[:config.sequenceLength])
+            embedding = bertClient.encode(get_split_text(content, config.split_len, 0)[:config.sequenceLength])
             print(embedding.shape)
             self.test_input_example.append(InputExample(embedding, label=lab))
             num += 1
         # 序列化
         writeDataFile(self.test_input_example, test_input_example_file)
 
-    def getLabelList(self):
+    def getLabelList(self, bertClient=None):
         if len(self.label_list) <= 0:
-            self.getTrainData()
+            self.getTrainData(bertClient if bertClient is not None else bc)
         return sorted(set(self.label_list), key=self.label_list.index)
 
-    def get_train_input_example(self):
+    def get_train_input_example(self, bertClient=None):
         if len(self.train_input_example) <= 0:
-            self.getTrainData()
+            self.getTrainData(bertClient if bertClient is not None else bc)
         return self.train_input_example
 
-    def get_eval_input_example(self):
+    def get_eval_input_example(self, bertClient=None):
         if len(self.eval_input_example) <= 0:
-            self.getValData()
+            self.getValData(bertClient if bertClient is not None else bc)
         return self.eval_input_example
 
-    def get_test_input_example(self):
+    def get_test_input_example(self, bertClient=None):
         if len(self.test_input_example) <= 0:
-            self.getTestData()
+            self.getTestData(bertClient if bertClient is not None else bc)
         return self.test_input_example
 
 
@@ -273,3 +274,17 @@ if __name__ == "__main__":
 
     text = u"彼此的眼神里充满了迷人的诱惑。突然，一长相还算不错的男子怒气冲冲的朝这对恋人走了过来，手里还提拎着个空的啤酒瓶。只见他扳过对方男子的肩，朝着那人的头一酒瓶子砸了下去，嘴里怒骂道:“李东强！！！我操你妈！敢抢我的女人？！”李东强被砸倒在地，额头上流下了鲜血。人群里顿时乱作一团，女人则发出了惊恐的尖叫声。见人被揍，同行的一行人纷纷围了过来。有人上前止住了那人的再次袭击。“温晁！你够了啊！”这名男子截住了温晁的拳头，挡在了李东强身前。这人大约二十岁出头，是一个长相十分干净帅气的小伙子，有着对明亮迷人的大眼睛，小麦般的健康肤色，修长健硕的体型，是那种让人一眼就会迷上的男人！“魏无羡！关你什么事？！你给我起开！”温晁甩开魏无羡的手，叫嚣道。魏无羡回头看了看倒在地上一脸痛苦的李东强，旁边的人正扶着他，查问着伤势。“江澄，先送他去医院。”魏无羡对好友江澄说道。“好！”江澄说着便在其他人的帮忙下扶起了李东强，朝酒吧外走去。东强的女友哭泣着跟了上去。温晁见状，气不打一出来，恶狠狠的恐吓道:“贱人！我跟你说，这事没完，我非弄死你们！”说着温晁便冲了过去。"
     # get_split_text(text, 5, 0)
+
+    # 创建线程
+    try:
+        # 创建并启动第一个线程
+        t1 = threading.Thread(target=data.get_train_input_example, args=([BertClient(ip='127.0.0.1', check_version=False, check_length=False)]))
+        t1.start()
+        # 创建并启动第二个线程
+        t2 = threading.Thread(target=data.get_eval_input_example, args=([BertClient(ip='127.0.0.1', check_version=False, check_length=False)]))
+        t2.start()
+        # 创建并启动第二个线程
+        t3 = threading.Thread(target=data.get_test_input_example, args=([BertClient(ip='127.0.0.1', check_version=False, check_length=False)]))
+        t3.start()
+    except:
+        print("Error: 无法启动线程")

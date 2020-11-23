f99168b2e8b88f9a68c620597516f6f818863e2d
zhonghong <zhonghong@liangzishidai.cn>
2020-11-23 19:23:34 +0800

    DNN

--- bert_with_lstm/train_DNNClassifier.py
+++ bert_with_lstm/train_DNNClassifier.py
@@ -8,6 +8,8 @@ from bert_with_lstm.dataset import *
 # Data sets
 labelList = data.getLabelList()
 
+print("labelList ==> ", labelList)
+
 train_example = data.get_train_input_example()
 
 eval_example = data.get_eval_input_example()
@@ -15,9 +17,10 @@ eval_example = data.get_eval_input_example()
 test_example = data.get_test_input_example()
 
 
-def wash_data(example):
-    perm = np.arange(len(example))
-    np.random.shuffle(perm)
+def wash_data(example, shuffle=True):
+    if shuffle:
+        perm = np.arange(len(example))
+        np.random.shuffle(perm)
 
     label_map = {}
     for (i, label) in enumerate(labelList):
@@ -38,46 +41,48 @@ def wash_data(example):
 
 
 def main():
-    feature_columns = [
-        tf.feature_column.numeric_column("embedding",
-                                         shape=[len(train_example), config.sequenceLength, config.model.embeddingSize])]
+    feature_columns = [tf.feature_column.numeric_column("embedding",
+                                                        shape=[config.sequenceLength, config.model.embeddingSize])]
 
     classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
-                                            hidden_units=[10, 20, 10],
+                                            hidden_units=[512, 256, 128],
                                             n_classes=20,
                                             dropout=0.5,
                                             model_dir="./output/dnn_model")
 
     x, y = wash_data(train_example)
+    print(x.shape, y.shape)
     train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"embedding": x}, y=y,
                                                         num_epochs=config.training.epoches, batch_size=config.batchSize,
                                                         shuffle=True)
 
-    classifier.train(input_fn=train_input_fn,
-                     max_steps=int(len(train_example) / config.batchSize * config.training.epoches))
+    classifier.train(input_fn=train_input_fn, )
 
     x, y = wash_data(eval_example)
     eval_input_fn = tf.estimator.inputs.numpy_input_fn(x={"embedding": x}, y=y,
                                                        num_epochs=1, batch_size=config.batchSize,
-                                                       shuffle=False)
+                                                       shuffle=True)
 
     evaluate_score = classifier.evaluate(input_fn=eval_input_fn, )
 
     print("evaluate_score ==> ", evaluate_score)
 
-    def new_samples():
-        example = []
-        right_label = []
-        for item in test_example:
-            example.append(padding(item.embedding))
-            right_label.append(item.label)
-        return np.array(example, dtype="float32")
+    pre_input_fn = tf.estimator.inputs.numpy_input_fn(x={"embedding": wash_data(test_example, False)[0]},
+                                                      num_epochs=1, batch_size=config.batchSize,
+                                                      shuffle=False)
 
-    predictions = list(classifier.predict(input_fn=new_samples))
+    result = classifier.predict(input_fn=pre_input_fn, yield_single_examples=False)
 
-    print("New Samples, Class Predictions:    {}n".format(predictions))
+    sum = 0
+    num = 0
+    for sam, prediction in zip(test_example, result):
+        # print("pre ==> ", prediction, " sam ==> ", sam.label)
+        sum += 1
+        class_ids = prediction['class_ids'][0][0]
+        if sam.label == labelList[class_ids]:
+            num += 1
+    print("测试准确率：", num / sum, sum, num)
 
 
 if __name__ == "__main__":
     main()
-
